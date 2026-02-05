e5c4208803346303d7a95bf616b14416d2fe9353
ShiloWexler <ShiloWexler@icloud.com>
2026-02-05 16:56:11 +0200

    Add entry point guard

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from contact_book_utils import *
-while True:
-    show_menu()
-    user_selection = (input("Please enter your choice: "))
-    if user_selection == 'quit':
-        break
-    menu_selection(user_selection)
+if __name__ == "__main__":
+    while True:
+        show_menu()
+        user_selection = input("Please enter your choice: ")
+        if user_selection == 'quit':
+            break
+        menu_selection(user_selection)
